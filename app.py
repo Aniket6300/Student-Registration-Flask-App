cea0df8444b493f5c33dfb38e0fd91ae5ba557ac
Aniket Yewale <yewalea00@gmail.com>
2026-04-05 01:39:33 +0530

    add

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request
+import mysql.connector
+
+app = Flask(__name__)
+
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'root',
+    'database': 'studentdb'
+}
+
+@app.route('/', methods=['GET', 'POST'])
+def register():
+
+    if request.method == 'POST':
+
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        course = request.form['course']
+        address = request.form['address']
+
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+
+        query = """
+        INSERT INTO students (name,email,phone,course,address)
+        VALUES (%s,%s,%s,%s,%s)
+        """
+
+        values = (name,email,phone,course,address)
+
+        cursor.execute(query, values)
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+        return "Student Registered Successfully"
+
+    return render_template('register.html')
+
+# Students List Page
+@app.route('/students')
+def students():
+
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM students"
+    cursor.execute(query)
+
+    data = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return render_template('students.html', students=data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
